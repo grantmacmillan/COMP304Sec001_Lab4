4470f62ac43cbc88ad7ee481a99fdd4f05d811e1
Ben Coombes <benzcoombes@gmail.com>
2022-11-13 18:19:59 -0500

    login and applicant finished

--- app/src/main/java/com/example/grantmacmillan_bencoombes_comp304sec001_lab4_ex1/ApplicantActivity.java
+++ app/src/main/java/com/example/grantmacmillan_bencoombes_comp304sec001_lab4_ex1/ApplicantActivity.java
@@ -6,36 +6,57 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ApplicantActivity extends AppCompatActivity {
-
+    AppDao doa;
     EditText applicantID;
     EditText firstName;
     EditText lastName;
     EditText testCenter;
     EditText examinerID;
-    RecyclerView recyclerView;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_applicant);
-
+        doa = AppDatabase.getInstance(this).appDao();
         applicantID = findViewById(R.id.applicantID);
         firstName = findViewById(R.id.firstName);
         lastName = findViewById(R.id.lastName);
         testCenter = findViewById(R.id.testCenter);
         examinerID = findViewById(R.id.examinerID);
-        recyclerView = findViewById(R.id.recyclerView);
+        listView = findViewById(R.id.listView);
     }
 
-    public void enterClicked(View v){
-
+    public void enterClicked(View v) {
+        int aId = Integer.parseInt(applicantID.getText().toString());
+        String fN = firstName.getText().toString();
+        String lN = lastName.getText().toString();
+        String tC = testCenter.getText().toString();
+        int eId = Integer.parseInt(examinerID.getText().toString());
+        Applicant applicant = new Applicant(aId, fN, lN, tC, eId);
+        doa.insertApplicant(applicant);
     }
 
-    public void viewClicked(View v){
+    public void viewClicked(View v) {
+        List<Applicant> applicantList = doa.getApplicants();
+        List<String> applicantView = new ArrayList<>();
 
+        for (int i = 0; i < applicantList.size(); i++) {
+            Applicant app = applicantList.get(i);
+            applicantView.add("Applicant ID: " + app.getApplicantId() + "\nName: " + app.getFirstName() + " " + app.getLastName() + "\nTest Center: " + app.getTestCenter() + "\nExaminer ID: " + app.getExaminerId());
+        }
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, applicantView);
+        listView.setAdapter(arrayAdapter);
     }
 
 
--- app/src/main/java/com/example/grantmacmillan_bencoombes_comp304sec001_lab4_ex1/LoginActivity.java
+++ app/src/main/java/com/example/grantmacmillan_bencoombes_comp304sec001_lab4_ex1/LoginActivity.java
@@ -6,22 +6,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText username;
     EditText password;
+    AppDao doa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        doa = AppDatabase.getInstance(this).appDao();
         username = findViewById(R.id.username);
         password = findViewById(R.id.password);
     }
 
     public void loginClicked(View v){
-
+        List<Examiner> examinerList = doa.getExaminers();
+        boolean boolLogin = false;
+        for(int i = 0; i < examinerList.size(); i++) {
+            int id = examinerList.get(i).examinerId;
+            String examinerPassword = examinerList.get(i).password;
+            if(Integer.parseInt(username.getText().toString()) == id) {
+                if(password.getText().toString().equals(examinerPassword)) {
+                    boolLogin = true;
+                }
+            }
+        }
+        if(boolLogin) {
+            Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        } else {
+            Toast.makeText(this, "Incorrect ID/Password", Toast.LENGTH_SHORT).show();
+        }
     }
 }
