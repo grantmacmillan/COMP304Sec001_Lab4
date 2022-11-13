2ec72829cdd8f35a8de23949a1d1b3403d5be914
grant <gmacmil3@my.centennialcollege.ca>
2022-11-13 17:27:01 -0500

    Test Activity Setup

--- app/src/main/java/com/example/grantmacmillan_bencoombes_comp304sec001_lab4_ex1/TestActivity.java
+++ app/src/main/java/com/example/grantmacmillan_bencoombes_comp304sec001_lab4_ex1/TestActivity.java
@@ -3,12 +3,34 @@ package com.example.grantmacmillan_bencoombes_comp304sec001_lab4_ex1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class TestActivity extends AppCompatActivity {
+
+    EditText testID;
+    EditText applicantID;
+    EditText examinerID;
+    EditText testResult;
+    EditText testDate;
+    EditText testRoute;
+    EditText testType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        testID = findViewById(R.id.testID);
+        applicantID = findViewById(R.id.applicantID);
+        examinerID = findViewById(R.id.examinerID);
+        testResult = findViewById(R.id.testResult);
+        testDate = findViewById(R.id.testDate);
+        testRoute = findViewById(R.id.testRoute);
+        testType = findViewById(R.id.testType);
+    }
+
+    public void EnterClicked(View v){
+
     }
 }
