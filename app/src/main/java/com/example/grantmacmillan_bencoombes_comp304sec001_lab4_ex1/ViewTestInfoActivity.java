efdf50f5f7f857d399eb0fe5f02a22e02b8b5a06
grant <gmacmil3@my.centennialcollege.ca>
2022-11-13 17:42:41 -0500

    Activity Setup Complete

--- app/src/main/java/com/example/grantmacmillan_bencoombes_comp304sec001_lab4_ex1/ViewTestInfoActivity.java
+++ app/src/main/java/com/example/grantmacmillan_bencoombes_comp304sec001_lab4_ex1/ViewTestInfoActivity.java
@@ -1,14 +1,27 @@
 package com.example.grantmacmillan_bencoombes_comp304sec001_lab4_ex1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class ViewTestInfoActivity extends AppCompatActivity {
+
+    EditText applicantID;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_test_info);
+
+        applicantID = findViewById(R.id.applicantID);
+        recyclerView = findViewById(R.id.recyclerView);
+    }
+
+    public void ViewClicked(View v){
+
     }
 }
