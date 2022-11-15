09e4129ba8ec943e136aeff825c92c67a19baa08
grant <gmacmil3@my.centennialcollege.ca>
2022-11-14 23:53:11 -0500

    fdsfdsf

--- app/src/main/java/com/example/grantmacmillan_bencoombes_comp304sec001_lab4_ex1/MainActivity.java
+++ app/src/main/java/com/example/grantmacmillan_bencoombes_comp304sec001_lab4_ex1/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         dao = AppDatabase.getInstance(this).appDao();
-        
+
     }
 
     public void button1Clicked(View v) {
